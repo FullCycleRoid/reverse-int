2f490b4c91eb834a12df807c3df559d5b9ddb775
FullCycleRoid <andrewthechf@gmail.com>
2020-10-09 11:20:11 +0300

    init: reverse int complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let num = String(n)
+  let temp = ''
+  if (num[0] === '-') {
+    return Number(num.substring(1).split('').reverse().join(''))
+  }
+  return Number(num.split('').reverse().join(''))
 }
